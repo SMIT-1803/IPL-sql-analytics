207410a6e00fb251c0a47a2e53c30885fe47c983
Smit Sachin Sanghvi <smitsachin1@gmail.com>
2026-07-02 23:07:51 -0700

    Add Orange Cap query: top scorer per season

---
+++ sql/queries/orange_cap.sql
@@ -0,0 +1,36 @@
+WITH season_runs AS (
+    SELECT
+        player_name,
+        SUM(runs_off_bat) AS runs,
+        season
+    FROM
+        fact_delivery
+        JOIN dim_player AS striker ON fact_delivery.striker_key = striker.player_key
+        JOIN dim_match AS match ON fact_delivery.match_id = match.match_id
+    GROUP BY
+        player_name,
+        season
+),
+ranked_runs AS (
+    SELECT
+        season_runs.player_name,
+        season_runs.runs,
+        season_runs.season,
+        RANK() OVER(
+            PARTITION BY season
+            ORDER BY
+                Runs DESC
+        ) AS player_rank
+    FROM
+        season_runs
+)
+SELECT
+    ranked_runs.player_name,
+    ranked_runs.Runs,
+    ranked_runs.season
+FROM
+    ranked_runs
+WHERE
+    player_rank = 1
+ORDER BY
+    season;
